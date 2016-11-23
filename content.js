6ef345605ee9152220fb22265c70c734c2975055
Loren Chorley <loren.chorley@gmail.com>
2016-11-23 10:14:08 +0100

    Initial

---
+++ content.js
@@ -0,0 +1,5 @@
+
+// Add the domain name and domain suffix to the html class string so that the css can use it
+var domainStrings = window.location.hostname.split(".").join(" ");
+var htmlclasses = document.documentElement.getAttribute("class");
+document.documentElement.setAttribute("class", htmlclasses + " " + domainStrings);
